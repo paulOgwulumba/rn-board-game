5a1d5430cc79049f44727c440c59868f60795c22
Paul Ogwulumba <pauleugeneogwulumba@gmail.com>
2022-03-04 13:57:10 +0000

    cell statuses

--- app/src/components/Board/BoardCell.js
+++ app/src/components/Board/BoardCell.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import { Pressable, View } from 'react-native';
 import styles from './Board.stylesheet';
+import { cellStatus } from '../../utils';
 
-const BoardCell = ({ cellPositionX = 0, cellPositionY = 0 }) => {
+const BoardCell = ({ cellPositionX = 0, cellPositionY = 0, status = cellStatus.EMPTY_CELL }) => {
     const [containsPiece, setContainsPiece] = useState(false);
     const [isHighlighted, setIsHighlighted] = useState(false);
 
---
+++ app/src/utils/cellStatus.js
@@ -0,0 +1,14 @@
+/**
+ * @description Status codes for the different cell statuses.
+ */
+const cellStatus = {
+    EMPTY_CELL: 0,
+    CONTAINS_PIECE_PLAYER_1: 1,
+    CONTAINS_PIECE_PLAYER_2: 2,
+    IS_SELECTED_PLAYER_1: 3,
+    IS_SELECTED_PLAYER_2: 4,
+    IS_A_HIT_PLAYER_1: 5,
+    IS_A_HIT_PLAYER_2: 6,
+};
+
+export { cellStatus };
